dabd8faeeb804f26836abe6bd1dc8c7cc0eba3a8
manhtdd <manh.td120901@gmail.com>
2024-03-25 09:31:51 +0700

    Update build script

--- build-run-dockerfile.sh
+++ build-run-dockerfile.sh
@@ -1,4 +1,4 @@
 #!/bib/bash
 
 docker build -t static-checker:vul4j .
-docker run -it --name static-checker-vul4j static-checker:vul4j
+docker run -it -v $(pwd):/app --name static-checker-vul4j static-checker:vul4j
